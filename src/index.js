356e059392835cde477174eb5e79b304a3eee185
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2023-03-13 19:09:36 -0400

    added google analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import ReactGA from 'react-ga';
 AOS.init()
+
+const TRACKING_ID = "G-4BMF2BMQDS";
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
